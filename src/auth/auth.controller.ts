6b62ba50eec7c7d02be8aafa4fd2d66c97dae51f
Amit Bar-Gil <amit217@yandex.com>
2023-01-01 21:06:41 +0200

    chore: merge from main

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,27 +1,12 @@
 import { Controller, Post, Body } from '@nestjs/common';
-import { OAuth2Client } from 'google-auth-library';
 import { AuthService } from './auth.service';
-
-const client = new OAuth2Client(
-  process.env.GOOGLE_CLIENT_ID,
-  process.env.GOOGLE_CLIENT_SECRET,
-);
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
-  async login(@Body('token') token): Promise<any> {
-    const ticket = await client.verifyIdToken({
-      idToken: token,
-      audience: process.env.GOOGLE_CLIENT_ID,
-    });
-    const { email, name } = ticket.getPayload();
-    const data = await this.authService.login({ email, name });
-    return {
-      data,
-      message: 'success',
-    };
+  async login(@Body('token') oAuthToken: string): Promise<unknown> {
+    return await this.authService.login(oAuthToken);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,27 @@
+import { OAuth2Client } from 'google-auth-library';
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+
+const client = new OAuth2Client(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+);
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  async login({ email, name }) {
-    return { email, name };
-  }
-  async test() {
-    return 'Hello';
+
+  async login(oAuthToken: string) {
+    try {
+      const ticket = await client.verifyIdToken({
+        idToken: oAuthToken,
+        audience: process.env.GOOGLE_CLIENT_ID,
+      });
+      const { email, name } = ticket.getPayload();
+      return {
+        userData: { email, name },
+        message: 'success',
+      };
+    } catch (error) {}
   }
 }
