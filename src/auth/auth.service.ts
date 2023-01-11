5ab89bc831ff972995bb994444bb48be70bd5be5
Amit Bar-Gil <96522800+AmitGit217@users.noreply.github.com>
2023-01-11 22:52:04 +0200

    Docs/edit readme (#35)
    * docs: starting workflow settings
    
    * chore: merge checkout
    
    * docs: add images of contributors to readme
    
    * fix(docs): add images of contributors to readme
    
    * docs: update link for contributors
    
    * fix(docs): take images from a generator
    
    * docs: fix ui
    
    * docs: welcome pr
    
    * refactor: delete ts dir
    
    * fix: delete config in auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -12,7 +12,6 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from '../prisma/prisma.service';
 
-
 @Injectable()
 export class AuthService {
   constructor(
