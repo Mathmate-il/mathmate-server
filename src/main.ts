551c14751444a65c773528a63b288d36cbf1eba9
Amit Bar-Gil <amit217@yandex.com>
2023-01-21 13:05:01 +0200

    refactor: bookmark model

--- src/main.ts
+++ src/main.ts
@@ -13,14 +13,35 @@ async function bootstrap() {
     origin: config.getConfig().app.cors.origin,
   });
   app.useGlobalPipes(new ValidationPipe());
-  app.use(helmet());
+  app.use(
+    helmet({
+      contentSecurityPolicy: {
+        directives: {
+          defaultSrc: ["'self'", 'https://oauth2.googleapis.com'],
+          connectSrc: ["'self'", 'https://oauth2.googleapis.com'],
+        },
+      },
+    }),
+  );
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Mathmate API')
     .setVersion('1.0')
+    .addOAuth2({
+      type: 'oauth2',
+      flows: {
+        clientCredentials: {
+          tokenUrl: 'https://oauth2.googleapis.com/token',
+          authorizationUrl: 'https://accounts.google.com/o/oauth2/auth',
+          scopes: {},
+        },
+      },
+    })
     .build();
+
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
+
   await app.listen(3001);
 
   console.log(
