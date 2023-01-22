e843f89f5682c3d4e9c19986cc42325a66d6c97b
Amit Bar-Gil <amit217@yandex.com>
2023-01-22 23:00:35 +0200

    chore: create user dto

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,9 @@
 import { JwtGuard } from './../auth/utils/auth.guard';
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from '@prisma/client';
 import { GetUser } from 'src/auth/utils/getUser.decorator';
+import { UserDto } from './dto/UserDto';
 
 @UseGuards(JwtGuard)
 @Controller('users')
@@ -10,7 +11,7 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get('/me')
-  async findOne(@GetUser() user: User): Promise<User> {
+  async findOne(@GetUser() user: UserDto): Promise<UserDto> {
     return user;
   }
 }
