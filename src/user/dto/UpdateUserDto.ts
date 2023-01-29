16d074c9b102ec928b3ee634733985ddb10619ee
Amit Bar-Gil <amit217@yandex.com>
2023-01-29 15:52:35 +0200

    refactor: get headers from decorator

--- src/user/dto/UpdateUserDto.ts
+++ src/user/dto/UpdateUserDto.ts
@@ -1,10 +1,12 @@
 import { UpdateErrorMessages } from './../../helpers/Errors.enums';
 import { NotUpdatable } from '../../helpers/NotUpdatable';
 import { IsOptional, IsDate } from '@nestjs/class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateUserDto {
   @IsOptional()
   @NotUpdatable({ message: UpdateErrorMessages.EmailNotUpdatable })
+  @ApiProperty()
   email?: string;
 
   @IsOptional()
@@ -12,13 +14,16 @@ export class UpdateUserDto {
 
   @IsOptional()
   @NotUpdatable({ message: UpdateErrorMessages.IdNotUpdatable })
+  @ApiProperty()
   id?: string;
 
   @IsOptional()
   @IsDate()
   @NotUpdatable({ message: UpdateErrorMessages.CreatedAtNotUpdatable })
+  @ApiProperty()
   createdAt?: Date;
 
   @IsOptional()
+  @ApiProperty()
   name?: string;
 }
