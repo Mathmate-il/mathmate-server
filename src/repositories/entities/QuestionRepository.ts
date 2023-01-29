934746171bc537c535598d0d4aec46674c4cb5e6
Amit Bar-Gil <amit217@yandex.com>
2023-01-29 14:19:14 +0200

    feat: validate tags in question creation

--- src/repositories/entities/QuestionRepository.ts
+++ src/repositories/entities/QuestionRepository.ts
@@ -1,3 +1,5 @@
+import { TagErrorMessages } from './../../helpers/Errors.enums';
+import { NotFoundException } from '@nestjs/common/exceptions';
 import { PrismaService } from '../../prisma/prisma.service';
 import { Repository } from '../repository';
 import { Injectable } from '@nestjs/common';
@@ -25,11 +27,23 @@ export class QuestionRepository extends Repository<
     question: CreateQuestionDto,
     userId: string,
   ): Promise<Question> {
+    const existingTags = await this.prisma.tag.findMany({
+      where: {
+        id: {
+          in: question.tags.map((tag) => tag.id),
+        },
+      },
+    });
+
+    if (existingTags.length !== question.tags.length) {
+      throw new NotFoundException(TagErrorMessages.NotFound);
+    }
+
     return this.prisma.question.create({
       data: {
         ...question,
         tags: {
-          create: [...question.tags],
+          connect: existingTags,
         },
         owner: {
           connect: {
