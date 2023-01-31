57224c8fe5f7b18f8a512aafaa53cf96ab18dcf1
Amit Bar-Gil <amit217@yandex.com>
2023-01-31 21:44:59 +0200

    fix: hot fix merge main

--- src/services/question/question.service.ts
+++ src/services/question/question.service.ts
@@ -1,16 +1,8 @@
-<<<<<<< HEAD:src/services/question/question.service.ts
 import { UserRepository } from '../../repositories/entities/UserRepository';
 import { ServerError } from '../../helpers/Errors.enums';
 import { NotFoundException } from '@nestjs/common/exceptions';
 import { CreateQuestionDto } from './dto/CreateQuestionDto';
 import { QuestionRepository } from '../../repositories/entities/QuestionRepository';
-=======
-import { UserRepository } from './../repositories/entities/UserRepository';
-import { ServerError } from './../helpers/Errors.enums';
-import { NotFoundException } from '@nestjs/common/exceptions';
-import { CreateQuestionDto } from './dto/CreateQuestionDto';
-import { QuestionRepository } from './../repositories/entities/QuestionRepository';
->>>>>>> main:src/question/question.service.ts
 import { Injectable, BadRequestException } from '@nestjs/common';
 
 @Injectable()
