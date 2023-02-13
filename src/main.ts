dec128e6c0948776804063da9e10578203eaca56
orig <48911144+origranot@users.noreply.github.com>
2023-02-13 23:26:33 +0200

    feat(logger): add global logger (#97)
    * feat(logger): add global logger
    
    * fix: conflict
    
    * feat(logger): update logger module
    
    * fix(main): fix conflict
    
    ---------
    
    Co-authored-by: orig <oriorigranot@gamil.com>

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,15 @@ import * as bodyParser from 'body-parser';
 import 'reflect-metadata';
 import config from './config/config.singleton';
 import { DatabaseSeeder } from './database/seeder';
+import { LOGGER_INJECTION_KEY } from './logger/logger.module';
+import { Logger } from '@origranot/ts-logger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const allExceptionsFilter = new AllExceptionsFilter();
   const publicPath = join(__dirname, '..', 'public');
   const viewsPath = join(__dirname, '..', 'views');
+  const logger = app.get<Logger>(LOGGER_INJECTION_KEY);
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
   app.use(allExceptionsFilter.writeApiRequestsLogToFile());
@@ -28,10 +31,10 @@ async function bootstrap() {
   await app.listen(config.appPort);
   const seeder = app.get(DatabaseSeeder);
   seeder.seedTagTable();
-  console.log(
+  logger.debug(
     '\x1b[1;34m 🚀 Swagger UI available at http://localhost:3000/swagger 🚀\x1b[0m',
   );
-  console.log(
+  logger.debug(
     '\x1b[1;34m 🔑 Google credentials available at http://localhost:3000/dev/google/auth 🔑\x1b[0m',
   );
 }
